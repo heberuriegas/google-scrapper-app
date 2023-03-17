852dd16afdd8a2c5cbc3de934a94a4eeba36f6f8
Heber <heber@inventati.org>
2023-03-17 06:44:57 -0600

    fix: remove header in sign in & sign up screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from "../features/auth/hooks/useAuth";
 
 export const Header = () => {
-  const { user, signOut } = useAuth();
+  const { signOut } = useAuth();
   return (
     <nav
       className="flex-no-wrap relative flex w-full items-center justify-between bg-neutral-100 py-4 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start"
@@ -33,32 +33,30 @@ export const Header = () => {
             </li>
           </ul>
         </div>
-        {user && (
-          <div className="relative flex items-center">
-            <button
-              className="mr-4 text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-              onClick={signOut}
-            >
-              <span className="[&>svg]:w-5 float-left">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-                  />
-                </svg>
-              </span>
-              <span>Sign out</span>
-            </button>
-          </div>
-        )}
+        <div className="relative flex items-center">
+          <button
+            className="mr-4 text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+            onClick={signOut}
+          >
+            <span className="[&>svg]:w-5 float-left">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                />
+              </svg>
+            </span>
+            <span>Sign out</span>
+          </button>
+        </div>
       </div>
     </nav>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { useAuth } from "../features/auth/hooks/useAuth";
 import { Header } from "./Header";
 
 /**
  * Main layout for UI, contains Header & Outlet
  * @returns {ReactElement}
  */
-export const Layout = () => (
-  <div data-testid="layout">
-    <Header />
-    <Outlet />
-  </div>
-);
+export const Layout = () => {
+  const { user } = useAuth();
+  return (
+    <div data-testid="layout">
+      {user && <Header />}
+      <Outlet />
+    </div>
+  );
+};
