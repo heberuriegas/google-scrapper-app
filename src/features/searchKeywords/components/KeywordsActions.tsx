7d756373bda397e8196979df3f66386f796b5165
Heber <heber@inventati.org>
2023-03-17 07:07:59 -0600

    feat: add csv keyword template

--- src/features/searchKeywords/components/KeywordsActions.tsx
+++ src/features/searchKeywords/components/KeywordsActions.tsx
@@ -1,9 +1,9 @@
 export const KeywordsActions = () => {
   return (
     <div className="flex mb-2 justify-end w-full">
-      <button className="rounded-lg p-2 mx-1 text-white bg-indigo-500">
+      <a href="/template.csv" download className="p-2 mx-1 text-indigo-500">
         Download CSV template
-      </button>
+      </a>
       <button className="rounded-lg p-2 mx-1 text-white bg-indigo-500">
         Load CSV file
       </button>
