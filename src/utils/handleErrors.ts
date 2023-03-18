b2b08769098bb0c2240eeb4a4170f1b599ac8e89
Heber Garza <heber.fernando@gmail.com>
2023-03-18 09:35:19 -0600

    docs: handleErrors utils

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -2,5 +2,10 @@ interface ValidationError {
   [key: string]: [string];
 }
 
+/**
+ * Check if a errors object was returned from an api call
+ * @param {any} err
+ * @returns {Object}
+ */
 export const responseErrors = (err: any) =>
   err?.response?.data?.errors as ValidationError;
